0fc35af86494226f4aea82268c8c7998bdc14bf3
LMurphy99 <lucasmurphy1999@gmail.com>
2017-03-10 14:44:27 +0000

    Add files via upload
    10/03/17

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import time
 import socket
 import threading
+import sys
+
+# Create alias for client
+alias = input("Name: ")
 
 # Create thread lock (ask Ross Milligan)
 tLock = threading.Lock()
@@ -10,13 +14,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect as client to selected server
 # on a specified port
-s.connect(('130.159.123.25',5000))
+s.connect(('130.159.123.8',5000))
 
-# Create alias for client
-alias = input("Name: ")
-
-# Tell server client has connected
-s.send((alias + " has connected.").encode("utf-8"))
+# Send server alias
+s.send(alias.encode("utf-8"))
 
 # Let client type message
 message = input(alias + "->")
@@ -29,6 +30,12 @@ while message != "END PROGRAM":
     message = input(alias + "->")
     tLock.release()
     time.sleep(0.2)
+
+    #ISSUE WITH 'CONNECT' WHEN TRYING TO RECEIVE DATA AS CLIENT
+    resp = (s.recv(2048)).strip()
+    decoded_resp = resp.decode("utf-8")
+    print(time.ctime()+": "+decoded_resp)
+            
 
         
 # Close the connection when completed
--- server.py
+++ server.py
@@ -1,48 +1,67 @@
 import socket
 import time
+import sys
+import threading
 
 # Specify server address and port
-host = '130.159.123.25'
+host = '130.159.123.8' 
 port = 5000
-
-print ("Server Started.")
+print('Host: ' + host + '\nPort: ' + str(port))
 
 clients = []
+print('Client list created')
 
 # Establish TCP/IP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
 
 # Bind server address and port
-s.bind((host,port))
+try:
+        s.bind((host,port))
+except socket.error as msg:
+        print('Bind failed. Error code: ' + str(msg[0]) + ' Message' + msg[1])
+        sys.exit()
+print('Socket bind complete')
 
 # Listen for x number of requests
 s.listen(5)
+print('Socket now listening')
 
-# Servers are constant loops handling requests
-connected = True
-while connected:
+# Function for handling connections.  This will be used to create threads
+def clientthread(connect):
+        # sending message to connected client
+        connect.send(('Welcome to the server!\n').encode('utf-8'))
 
-    # Wait for a connection
-    connect, address = s.accept()
+        # infinite loop so that function do not terminate and thread do not end
+        while True:
 
-    # Typically fork at this point
- 
-    # Specify max bytes to be received
-    resp = (connect.recv(2048)).strip()
-    # Add clients to address book
-    if address not in clients:
-        clients.append(address)
+                # Typically fork at this point
+     
+                # Specify max bytes to be received
+                resp = (connect.recv(2048)).strip()
+                # Add clients to address book
+                if connect not in clients:
+                        clients.append(connect)
 
-    # Send answer
-    decoded_resp = resp.decode("utf-8")
-    print(time.ctime()+": "+decoded_resp)
-    
+                # Send answer
+                decoded_resp = resp.decode("utf-8")
+                print(time.ctime()+": "+decoded_resp)
+                for conn in clients:
+                        conn.send(resp)
 
-# Close connection when finished
-connect.close()
-print("\ndone"),address
+                
+        # Close connection when finished
+        connect.close()
+        print("\ndone"+address)
 
-
- 
-
-
+# now keep talking with the client
+while 1:
+        # wait to accept a connection - blocking call
+        connect, address = s.accept()
+        alias = ((connect.recv(512)).strip()).decode('utf-8')
+        print(time.ctime()+ ': ' + address[0] + ' (' + alias + ')' + ' has connected.')
+        #start new thread takes 1st argument as a function name to be run,
+        #second is the tuple of arguments to the function.
+        threading.Thread(target=clientthread,args=(connect, )).start()
+        
+s.close
